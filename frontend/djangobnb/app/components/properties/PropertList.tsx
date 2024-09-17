25ffbec37c29b0b0b41884d86a251b3fc6420692
Juliusz <julek.winn@gmail.com>
2024-09-18 01:13:32 +0200

    refactor: Update URLs and settings for media files

--- frontend/djangobnb/app/components/properties/PropertList.tsx
+++ frontend/djangobnb/app/components/properties/PropertList.tsx
@@ -1,10 +1,38 @@
+"use client";
+import { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItem";
+
+export type PropertyType = {
+  id: string;
+  title: string;
+  price_per_night: number;
+  image_url: string;
+};
+
 const PropertyList = () => {
+  const [properties, setProperties] = useState<PropertyType[]>([]);
+
+  const getProperties = async () => {
+    const url = "http://localhost:8000/api/properties/";
+    try {
+      const response = await fetch(url, { method: "GET" });
+      const responseData = await response.json();
+      console.log(responseData);
+      setProperties(responseData.data);
+    } catch (error) {
+      console.error("Błąd podczas pobierania danych:", error);
+    }
+  };
+
+  useEffect(() => {
+    getProperties();
+  }, []);
+
   return (
     <>
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
+      {properties.map((property) => (
+        <PropertyListItem key={property.id} property={property} />
+      ))}
     </>
   );
 };
--- frontend/djangobnb/app/components/properties/PropertyListItem.tsx
+++ frontend/djangobnb/app/components/properties/PropertyListItem.tsx
@@ -1,23 +1,28 @@
 import Image from "next/image";
+import { PropertyType } from "./PropertList";
 
-const PropertyListItem = () => {
+interface PropertyProps {
+  property: PropertyType;
+}
+
+const PropertyListItem: React.FC<PropertyProps> = ({ property }) => {
   return (
     <div className="cursor-pointer">
       <div className="relative overflow-hidden aspect-square rounded-xl">
         <Image
           fill
-          src="/Beach_house.jpg"
+          src={property.image_url}
           sizes="(max-width:768px) 768px, (max-width: 1200px): 768px, 768px"
           className="hover:scale-110 object-cover transition h-full w-full"
           alt="Beach house"
         ></Image>
       </div>
       <div className="mt-2">
-        <p className="text-lg font-bold">Property Name</p>
+        <p className="text-lg font-bold">{property.title}</p>
       </div>
       <div className="mt-2">
         <p className="text-sm text-gray-500">
-          <strong>$200</strong> per night
+          <strong>${property.price_per_night}</strong> per night
         </p>
       </div>
     </div>
