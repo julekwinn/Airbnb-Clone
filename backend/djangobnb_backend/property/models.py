4577648ee5fc2bb2b9271bd4e7c9de6b15756968
Juliusz <julek.winn@gmail.com>
2024-09-18 00:15:12 +0200

    refactor: Update entrypoint.sh and models.py
    - Update entrypoint.sh to include a missing command for making migrations.
    - Add a new model Property to models.py with various fields for property details.

--- backend/djangobnb_backend/property/models.py
+++ backend/djangobnb_backend/property/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid
+from django.conf import settings
+from useraccount.models import User
 
-# Create your models here.
+class Property(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    price_per_night = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    guests = models.IntegerField()
+    country = models.CharField(max_length=255)
+    country_code = models.CharField(max_length=10)
+    category = models.CharField(max_length=255)
+    
+    #favorited
+    image = models.ImageField(upload_to='uploads/properties/')
+    landlord = models.ForeignKey(User, related_name='properties', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
