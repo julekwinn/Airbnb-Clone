79b521b0f194ab785bf5ca071a77a1708011ed9e
Juliusz <julek.winn@gmail.com>
2024-09-18 23:18:30 +0200

    refactor: Update LoginModal component to include login functionality
    - Added useRouter hook from next/navigation for routing
    - Imported apiServices from "@/app/services/apiService" for making API requests
    - Imported handleLogin function from "@/app/services/actions" for handling successful login
    - Added useState hooks for email, password, and errors
    - Implemented submitLogin function to handle form submission
    - Added form inputs for email and password
    - Displayed error messages if login fails
    - Added submit button to trigger login process

--- frontend/djangobnb/app/components/modals/LoginModal.tsx
+++ frontend/djangobnb/app/components/modals/LoginModal.tsx
@@ -2,33 +2,61 @@
 import CustomButton from "../forms/CustomButton";
 import Modal from "./Modal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { useRouter } from "next/navigation";
+import apiServices from "@/app/services/apiService";
+import { handleLogin } from "@/app/services/actions";
+import { useState } from "react";
+
 const LoginModal = () => {
+  const router = useRouter();
   const loginModal = useLoginModal();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const submitLogin = async () => {
+    const formData = {
+      email: email,
+      password: password,
+    };
+
+    const response = await apiServices.post("/api/auth/login/", formData);
+
+    if (response.access) {
+      handleLogin(response.user.pk, response.access, response.refresh);
+      loginModal.close();
+      router.push("/");
+    } else {
+      setErrors(response.non_field_errors);
+    }
+  };
 
   const content = (
     <>
       {" "}
       <h2 className="mb-6 text-2xl">Welcome to Djangobnb, please log in</h2>
-      <form className="space-y-4">
+      <form className="space-y-4" action={submitLogin}>
         <input
+          onChange={(e) => setEmail(e.target.value)}
           type="email"
           className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"
           placeholder="Your email address"
         />
         <input
+          onChange={(e) => setPassword(e.target.value)}
           type="password"
           className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"
           placeholder="Your password"
         />
-        <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-          Error message
-        </div>
-        <CustomButton
-          label="Submit"
-          onClick={() => {
-            console.log("Button submitted pressed");
-          }}
-        />{" "}
+        {errors.map((error, index) => (
+          <div
+            key={`error_${index}`}
+            className="p-5 bg-airbnb text-white rounded-xl opacity-80"
+          >
+            {error}
+          </div>
+        ))}
+        <CustomButton label="Submit" onClick={submitLogin} />{" "}
       </form>
     </>
   );
